64c977266404c555f8646f462dc0356d76476b26
WaqasRasheed14 <waqasrasheed14@gmail.com>
2020-11-17 15:51:17 +0200

    config changed

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_RYmtHvYTa', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4ct43spuc264p5iaoc4coclc6f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
